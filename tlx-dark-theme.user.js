1778b3970837b06a1803dc003d4d0de0724cd458
juancarlovieri <juan.c.vieri@gmail.com>
2020-09-26 14:43:14 +0700

    change version

--- tlx-dark-theme.user.js
+++ tlx-dark-theme.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         tlx dark theme
-// @version      2.1.3
+// @version      2.1.4
 // @description  dark theme for tlx
 // @author       Juan Carlo Vieri
 // @match        *://tlx.toki.id/*
