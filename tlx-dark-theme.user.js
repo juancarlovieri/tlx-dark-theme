d53ec13ea905477c60f6b9fb56f1673f221428f5
Juan Carlo Vieri <juan.c.vieri@gmail.com>
2021-06-19 09:12:40 +0700

    change version

--- tlx-dark-theme.user.js
+++ tlx-dark-theme.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         tlx dark theme
-// @version      2.9.9
+// @version      3.0.0
 // @description  dark theme for tlx
 // @author       Juan Carlo Vieri
 // @match        *://tlx.toki.id/*
